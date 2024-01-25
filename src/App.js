5692f2a7b7163869aee41ee51d2944a9f20f1420
JonnyG8 <dotavo111@gmail.com>
2024-01-25 15:07:53 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {useState, useEffect} from 'react';
 
 function App() {
+  const [number, setNumber] = useState(null);
+  const [number2, setNumber2] = useState(null);
+  const [plusResult, setPlusResult] = useState(null);
+
+  let firstValue = (event) => {
+    setNumber(event.target.value);
+  };
+
+  let secondValue = (event) => {
+    setNumber2(event.target.value);
+  }
+
+  let plusNumbers = () => {
+    setPlusResult(parseFloat(number) + parseFloat(number2));
+  }
+
+
+  useEffect(() => {
+    if(parseFloat(number) === 0 && parseFloat(number2) === 0) {
+      alert('The inputted numbers can not be 0')
+    }
+  }, [number, number2]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        onChange={firstValue}
+        placeholder='input the number'
+      >
+      </input>
+      <input
+        onChange={secondValue}
+        placeholder='input the number'
+      >
+      </input>
+      <button onClick={plusNumbers}>
+        +
+      </button>
+      <div>
+        <h2>{plusResult}</h2>
+      </div>
     </div>
   );
 }
